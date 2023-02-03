6ebd338cefe725dc0545e140ef35c657173aa070
Davron <davronshirinboyev1@gmail.com>
2023-02-03 10:17:01 +0500

    new file
    new file

--- untitled1.py
+++ untitled1.py
@@ -5,4 +5,45 @@ Created on Mon Jan 30 15:50:32 2023
 @author: DAVRON
 """
 
-print("Bismilahir Rahmonir Rohiym")
+otam ={"ism":"raxmatillo","familiyasi":"ro/'ziqulov","yosh":"47","t_yili":"1975-yil"}
+
+
+
+
+print(f"Otamning ismi  {otam['ism'].title()},\
+ {otam['t_yili']}-yilda tu'gilgan,\
+ {otam['yosh']} yoshda")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
