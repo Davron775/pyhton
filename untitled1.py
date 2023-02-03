24995d60b311fee77c60b70030873c08e3cd67a5
Davron <davronshirinboyev1@gmail.com>
2023-02-03 16:05:59 +0500

    there is a new change
    there is a new change

--- untitled1.py
+++ untitled1.py
@@ -2,21 +2,24 @@
 """
 Created on Mon Jan 30 15:50:32 2023
 
-@author: DAVRON
+@author: DAVRON  Amalyotlarim
 """
 
-otam ={"ism":"raxmatillo","familiyasi":"ro/'ziqulov","yosh":"47","t_yili":"1975-yil"}
+#otam ={"ism":"raxmatillo","familiyasi":"ro/'ziqulov","yosh":"47","t_yili":"1975-yil"}
 
 
 
 
-print(f"Otamning ismi  {otam['ism'].title()},\
- {otam['t_yili']}-yilda tu'gilgan,\
- {otam['yosh']} yoshda")
+#print(f"Otamning ismi  {otam['ism'].title()},\
+# {otam['t_yili']}-yilda tu'gilgan,\
+# {otam['yosh']} yoshda")
 
 
-
-
+#men ={"ism":"davron","yosh":22,"kasb":"dasturchi","maqsad":"OLLOHNING_YUZINI_KO'RISH"}
+#print(f"Mening ismim {men['ism'].title()},\
+#mening yoshim {men['yosh']}da,\
+#Kasbim {men['kasb']}")
+    
 
 
 
