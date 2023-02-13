c9aeb08acd87b31e79207b165811e42a9298b560
Davron <davronshirinboyev1@gmail.com>
2023-02-13 14:33:05 +0500

    Turtle  Fleur
    Foto

--- turtle.animatsiya.py
+++ turtle.animatsiya.py
@@ -2,22 +2,62 @@
 """
 Created on Mon Feb 13 13:01:39 2023
 
-Animatsiya
+Anim"atsiya
 
-@author: DAVRON
+@author:" DAVRON
 """
 
-import turtle
+"""import turtle
 
 t = turtle.Turtle()
 turtle.bgcolor("black")
-t.speed(0)
+t.speed(0.5)
 temp = 1
-c = ["red","green","blue"]
-for i in range(400):
+c = ["blue","green","white"]
+for i in range(500):
     t.color(c[i%3])
     t.forward(temp)
     t.left(120)
     t.left(1)
     temp=temp+1
 turtle.mainlop()
+"""
+from turtle import *
+
+ def fleur():
+     for i in range(300):
+         circle(190-i,90)
+         left(90)
+         circle(190-i,90)
+         left(18)
+         speed(0.5)
+         
+         
+fleur()
+mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
